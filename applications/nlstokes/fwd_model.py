debb589aed3c1f42fd55980a416e37736b78d0d7
Umberto Villa <uvilla@ices.utexas.edu>
2016-03-24 16:41:49 -0500

    Add copyright information on each file

--- applications/nlstokes/fwd_model.py
+++ applications/nlstokes/fwd_model.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2016, The University of Texas at Austin & University of
+# California, Merced.
+#
+# All Rights reserved.
+# See file COPYRIGHT for details.
+#
+# This file is part of the hIPPYlib library. For more information and source code
+# availability see https://hippylib.github.io.
+#
+# hIPPYlib is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License (as published by the Free
+# Software Foundation) version 2.1 dated February 1999.
+
+# See: 
 #J. Freund, R. Stenberg. On weakly imposed boundary conditions for second order problems.
 #Proceedings of the Ninth Int. Conf. Finite Elements in Fluids, Venice 1995.
 #M. Morandi Cecchi et al., Eds. pp. 327-336
--- hippylib/unit_test/PointwiseObservation.py
+++ hippylib/unit_test/PointwiseObservation.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2016, The University of Texas at Austin & University of
+# California, Merced.
+#
+# All Rights reserved.
+# See file COPYRIGHT for details.
+#
+# This file is part of the hIPPYlib library. For more information and source code
+# availability see https://hippylib.github.io.
+#
+# hIPPYlib is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License (as published by the Free
+# Software Foundation) version 2.1 dated February 1999.
+
 from dolfin import *
 import sys
 sys.path.append( "../../" )
