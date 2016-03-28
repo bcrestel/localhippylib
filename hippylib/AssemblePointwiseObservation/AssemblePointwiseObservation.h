3102dc9a65a4fda24b50a1d980f5811fb9ae1d85
Umberto Villa <uvilla@ices.utexas.edu>
2016-03-28 18:21:24 -0500

    Fix GPL version in Copyright

--- hippylib/AssemblePointwiseObservation/AssemblePointwiseObservation.h
+++ hippylib/AssemblePointwiseObservation/AssemblePointwiseObservation.h
@@ -9,7 +9,7 @@
  *
  * hIPPYlib is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License (as published by the Free
- * Software Foundation) version 2.1 dated February 1999.
+ * Software Foundation) version 3.0 dated June 2007.
 */
 
 #include <dolfin/function/FunctionSpace.h>
