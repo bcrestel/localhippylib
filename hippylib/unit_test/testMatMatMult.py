9e25d978416ba98b630a3693be9c6a99fc405170
Umberto Villa <uvilla@ices.utexas.edu>
2016-10-13 12:20:35 -0500

    Make methods in linalg.cpp return std::shared_ptr instead of objects

--- hippylib/unit_test/testMatMatMult.py
+++ hippylib/unit_test/testMatMatMult.py
@@ -24,8 +24,6 @@ mesh = dl.UnitSquareMesh(nx, ny)
 
 Vh = dl.FunctionSpace(mesh, 'Lagrange', 1)
 
-data = np.ones(Vh.dim(), dtype=np.float64)
-
 uh = dl.TrialFunction(Vh)
 vh = dl.TestFunction(Vh)
 
@@ -34,52 +32,5 @@ B = dl.assemble(dl.inner(dl.nabla_grad(uh), dl.nabla_grad(vh))*dl.dx)
 
 C = MatMatMult(A,B)
 D = MatPtAP(A,B)
-
-
-x = dl.Vector()
-y = dl.Vector()
-xA = dl.Vector()
-yA = dl.Vector()
-xD = dl.Vector()
-yD = dl.Vector()
-
-A.init_vector(yA,0)
-A.init_vector(xA,1)
-
-C.init_vector(y,0)
-C.init_vector(x,1)
-
-D.init_vector(yD,0)
-D.init_vector(xD,1)
-
-print "y in range of A: y.set_local(data)..."
-yA.set_local(data)
-print "... success"
-
-print "x in domain of A: x.set_local(data)..."
-xA.set_local(data)
-print "... success"
-
-print "x in domain of C: x.set_local(data)..."
-x.set_local(data)
-print "... success"
-
-print "x in domain of C: x.set_local(data)..."
-x.set_local(data)
-print "... success"
-
-print "y in range of C: y.set_local(data)..."
-y.set_local(data)
-print "... success"
-
-print "x in domain of C: x.set_local(data)..."
-x.set_local(data)
-print "... success"
-
-print "y in range of D: y.set_local(data)..."
-yD.set_local(data)
-print "... success"
-
-print "x in domain of D: x.set_local(data)..."
-xD.set_local(data)
-print "... success"
+E = MatAtB(A,B)
+F = Transpose(A)
