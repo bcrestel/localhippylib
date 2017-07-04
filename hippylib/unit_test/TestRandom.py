47d54a0d74117a0d9757744b2d202d396666b1d8
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2017-07-04 11:51:26 -0500

    minor

--- hippylib/unit_test/TestRandom.py
+++ hippylib/unit_test/TestRandom.py
@@ -11,11 +11,15 @@ mesh = dl.UnitSquareMesh(nx, ny)
 rank = dl.MPI.rank(mesh.mpi_comm())
 nproc = dl.MPI.size(mesh.mpi_comm())
 
-Random.split(rank, nproc, 10, 1)
+#Random.split(rank, nproc, 10, 1)
 
+#np.random.seed(1)
+pp=np.random.RandomState(1)
 z = np.zeros(20)
 for i in np.arange(z.shape[0]):
     z[i] = Random.normal(0., 1.)
+    #z[i] = np.random.randn(1)
+    #z[i] = pp.standard_normal(1)
     
-print "PID {0}:".format(rank), z
+print "PID {0}:".format(rank), z, z.mean(), z.std()
     
