8f199d9a14e3513cf7f843699f93e49ae2d6ff25
Benjamin Crestel <crestel@ices.utexas.edu>
2017-07-26 17:07:54 -0500

    continue implementation jointmodeli--needs to be checked

--- hippylib/jointmodel.py
+++ hippylib/jointmodel.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import dolfin as dl
 
@@ -326,7 +327,7 @@ class JointModeli:
             try:
                 self.splitassign = [None, None, None]
                 for cc in [STATE, ADJOINT, PARAMETER]:
-                    self.splitassign[cc] = SplitAndAssigni(Vhs[cc], self.M[cc].mpi_comm())
+                    self.splitassign[cc] = SplitAndAssigni(Vhs[cc])
                 if self.parameters['print']:
                     print '[JointModel] Using SplitAndAssigni'
             except:
@@ -354,31 +355,36 @@ class JointModeli:
             if self.parameters['splitassign'] == True:
                 return self.splitassign[component].split(x)
             else:
-                fun = vector2Function(x, self.Vh[component])
-                funs = fun.split(deepcopy=True)
-                funsout = []
-                for ff in funs:
-                    funsout.append(funs.vector())
-                return funsout
+                sys.exit(1)
+#                fun = vector2Function(x, self.Vh[component])
+#                funs = fun.split(deepcopy=True)
+#                funsout = []
+#                for ff in funs:
+#                    funsout.append(funs.vector())
+#                return funsout
 
-    #TODO: continue here
-    def assignvector(self, x1, x2, component="ALL"):
-        """ Create a single vector x from (x1, x2) """
+
+    def assignvector(self, xs, component="ALL"):
+        """ Create a single vector x from (x1,..,xn) """
         if component == "ALL":
             x = [None, None, None]
             for cc in [STATE, ADJOINT, PARAMETER]:
-                x[cc] = self.assignvector(x1[cc], x2[cc], cc)
+                xscc = []
+                for xsii in xs:
+                    xscc.append(xsii[cc])
+                x[cc] = self.assignvector(xscc, cc)
             return x
         else:
             if self.parameters['splitassign'] == True:
-                return self.splitassign[component].assign(x1, x2)
+                return self.splitassign[component].assign(xs)
             else:
-                fun1 = vector2Function(x1, self.model1.problem.Vh[component])
-                fun2 = vector2Function(x2, self.model2.problem.Vh[component])
-                fun = dl.Function(self.Vh[component])
-                dl.assign(fun.sub(0), fun1)
-                dl.assign(fun.sub(1), fun2)
-                return fun.vector()
+                sys.exit(1)
+#                fun1 = vector2Function(x1, self.model1.problem.Vh[component])
+#                fun2 = vector2Function(x2, self.model2.problem.Vh[component])
+#                fun = dl.Function(self.Vh[component])
+#                dl.assign(fun.sub(0), fun1)
+#                dl.assign(fun.sub(1), fun2)
+#                return fun.vector()
 
 
     def generate_vector(self, component="ALL"):
@@ -399,7 +405,6 @@ class JointModeli:
         else:
             x = dl.Vector()
             self.M[component].init_vector(x, 0)
-
         return x
             
 
@@ -410,26 +415,26 @@ class JointModeli:
 
     def solveFwd(self, out, x, tol=1e-9):
         """ Solve the forward problem  for each inverse problem """
-        out1, out2 = self.splitvector(out, STATE)
-        x1, x2 = self.splitvector(x, "ALL")
+        outs = self.splitvector(out, STATE)
+        xs = self.splitvector(x, "ALL")
 
-        self.model1.solveFwd(out1, x1, tol)
-        self.model2.solveFwd(out2, x2, tol)
-
+        for modelii, outii, xsii in zip(self.models, outs, xs):
+            self.modelii.solveFwd(outii, xsii, tol)
+    
         out.zero()
-        out.axpy(1.0, self.assignvector(out1, out2, STATE))
+        out.axpy(1.0, self.assignvector(outs, STATE))
 
 
     def solveAdj(self, out, x, tol=1e-9):
         """ Solve the adjoint problem for each inverse problem """
-        out1, out2 = self.splitvector(out, ADJOINT)
-        x1, x2 = self.splitvector(x, "ALL")
+        outs = self.splitvector(out, ADJOINT)
+        xs = self.splitvector(x, "ALL")
 
-        self.model1.solveAdj(out1, x1, tol)
-        self.model2.solveAdj(out2, x2, tol)
-
+        for modelii, outii, xsii in zip(self.models, outs, xs):
+            self.modelii.solveAdj(outii, xsii, tol)
+    
         out.zero()
-        out.axpy(1.0, self.assignvector(out1, out2, ADJOINT))
+        out.axpy(1.0, self.assignvector(outs, ADJOINT))
 
 
     def cost(self, x):
@@ -442,13 +447,14 @@ class JointModeli:
         
         Note: p is not needed to compute the cost functional
         """        
-        x1, x2 = self.splitvector(x, "ALL")
+        xs = self.splitvector(x, "ALL")
+        misfit = 0.0
 
-        _, _, misfit1 = self.model1.cost(x1)
-        _, _, misfit2 = self.model2.cost(x2)
-        misfit = misfit1 + misfit2
+        for modelii, xsii in zip(self.models, xs):
+            _, _, misfitii = self.modelii.cost(xsii)
+            misfit += misfitii
 
-        reg = self.Prior.costabvect(x1[PARAMETER], x2[PARAMETER])
+        reg = self.Prior.costabvect(x[PARAMETER])
 
         return misfit + self.alphareg*reg, reg, misfit
 
@@ -463,21 +469,19 @@ class JointModeli:
         
         Returns the norm of the gradient in the correct inner product g_norm = sqrt(g,g)
         """ 
-        x1, x2 = self.splitvector(x, "ALL")
-        if self.parameters['splitassign']:
-            mg1 = self.model1.generate_vector(PARAMETER)
-            mg2 = self.model2.generate_vector(PARAMETER)
-        else:
-            mg1, mg2 = self.splitvector(mg, PARAMETER)
-
-        g_n1 = self.model1.evalGradientParameter(x1, mg1)
-        g_n2 = self.model2.evalGradientParameter(x2, mg2)
-        if self.parameters['print']:
-            print '||g1||={}, ||g2||={}'.format(g_n1, g_n2)
+        xs = self.splitvector(x, "ALL")
+        mgs = []
+        for modelii, xsii in zip(self.models, xs):
+            mgii = modelii.generate_vector(PARAMETER)
+            g_nii = modelii.evalGradientParameter(xsii, mgii)
+            if self.parameters['print']:
+                print '||g||={},'.format(g_nii),
+            mgs.append(mgii)
+        if self.parameters['print']:    print ''
 
         mg.zero()
-        mg.axpy(1.0, self.assignvector(mg1, mg2, PARAMETER))
-        mg.axpy(self.alphareg, self.Prior.gradabvect(x1[PARAMETER], x2[PARAMETER]))
+        mg.axpy(1.0, self.assignvector(mgs, PARAMETER))
+        mg.axpy(self.alphareg, self.Prior.gradabvect(xs[PARAMETER])
 
         g = self.generate_vector(PARAMETER)
         self.Msolver.solve(g, mg)
@@ -491,36 +495,32 @@ class JointModeli:
         Specify the point x = [u,a,p] at which the Hessian operator (or the Gauss-Newton approximation)
         need to be evaluated.
         """      
-        x1, x2 = self.splitvector(x, "ALL")
+        xs = self.splitvector(x, "ALL")
+        for modelii, xsii in zip(self.models, xs):
+            modelii.setPointForHessianEvaluations(xsii)
 
-        self.model1.setPointForHessianEvaluations(x1)
-        self.model2.setPointForHessianEvaluations(x2)
-
-        self.Prior.assemble_hessianab(x1[PARAMETER], x2[PARAMETER])
+        self.Prior.assemble_hessianab(xs[PARAMETER])
 
 
     def mult(self, x, y):
         """
         Compute Hessian-vector product with x, and save it to y
         """
-        x1, x2 = self.splitvector(x, PARAMETER)
-        y1, y2 = self.splitvector(y, PARAMETER)
+        xs = self.splitvector(x, PARAMETER)
+        ys = self.splitvector(y, PARAMETER)
 
-        # model 1
-        H1 = ReducedHessian(self.model1, self.tol, self.GN, True)
-        H1.mult(x1, y1)
-        # model 2
-        H2 = ReducedHessian(self.model2, self.tol, self.GN, True)
-        H2.mult(x2, y2)
-        # combine them
+        for modelii, xsii, ysii in zip(self.models, xs, ys):
+            Hii = ReducedHessian(modelii, self.tol, self.GN, True)
+            #TODO: check that ysii is updated
+            Hii.mult(xsii, ysii)
+
         y.zero()
-        y.axpy(1.0, self.assignvector(y1, y2, PARAMETER))
+        y.axpy(1.0, self.assignvector(ys, PARAMETER))
 
 
     def applyR(self, da, out):
-        da1, da2 = self.splitvector(da, PARAMETER)
         out.zero()
-        out.axpy(self.alphareg, self.Prior.hessianab(da1, da2))
+        out.axpy(self.alphareg, self.Prior.hessianab(da))
 
         
     def Rsolver(self):        
@@ -528,10 +528,18 @@ class JointModeli:
 
 
     def mediummisfit(self, a):
-        a1, a2 = self.splitvector(a, PARAMETER)
-        nd1, nd1p = self.model1.mediummisfit(a1)
-        nd2, nd2p = self.model2.mediummisfit(a2)
-        return 0.5*(nd1+nd2), 0.5*(nd1p+nd2p)
+        a_s = self.splitvector(a, PARAMETER)
+        nd = 0.0
+        ndp = 0.0
+        for modelii, a_sii in zip(self.models, a_s):
+            ndii, ndiip = modelii.mediummisfit(a_sii)
+            nd += ndii
+            ndp += ndiip
+        return 0.5*nd, 0.5*ndp
+
 
     def getPDEcounts(self):
-        return self.model1.getPDEcounts() + self.model2.getPDEcounts()
+        PDEc = 0.0
+        for modelii in self.models:
+            PDEc += modelii.getPDEcounts()
+        return PDEc
